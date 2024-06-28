38e4e9257dd0e96305809f558dcf37842c109f2b
BabcB <85190284+BabcB@users.noreply.github.com>
2024-06-28 17:14:50 +0000

    basic style applies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [bool, setBool] = useState(true);
+
+  const handlePress = () => {
+    setBool((prev) => !prev);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <StatusBar style="auto" backgroundColor={bool ? 'pink' : 'green'} />
+      <View style={styles.container}>
+        <Text>{String(bool)}</Text>
+        <View style={styles.buttonContainer}>
+          <Button onPress={handlePress} title="Click me" />
+          <Button onPress={handlePress} title="Click me" />
+          <Button onPress={handlePress} title="Click me" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container: {
+    display: 'flex',
+    gap: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: { display: 'flex', flexDirection: 'row', gap: 10 },
 });
